def285d7f091c2cee6457bef098b4c3771efd01e
wijnandK <korlaarwijnand@hotmail.com>
2021-11-15 23:59:59 +0100

    A

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,21 +1,56 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
 function PostContent(props) {
   const { post } = props;
-  console.log(props);
-  const imagepath = `/images/posts/${post.slug}/${post.image}`;
+
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
-    img(image) {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
       return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          width={300}
-          alt={image.alt}
-          height={300}
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
         />
       );
     },
@@ -23,9 +58,10 @@ function PostContent(props) {
 
   return (
     <article className={classes.content}>
-      <PostHeader title={post.title} image={imagepath} />
-      <ReactMarkdown components={customRenderers} children={post.content} />
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
+
 export default PostContent;
