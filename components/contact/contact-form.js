f4a1b32a2a57459fbd3869ed00ab926ce44b5720
wijnandK <korlaarwijnand@hotmail.com>
2021-11-16 15:33:09 +0100

    Message

---
+++ components/contact/contact-form.js
@@ -0,0 +1,19 @@
+import classes from "./contact-form-module.css";
+
+function ContactForm() {
+  return (
+    <section className={classes.contact}>
+      <h1>How can i help you ?</h1>
+      <form className={classes.form}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" required />{" "}
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
